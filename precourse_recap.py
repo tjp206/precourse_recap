7f65656ceee4ee859308e087c6f1f53b4ec18d77
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-08-31 16:53:38 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,12 @@
+# IF name == 'TJ' 
+# PRINT 'You got it ya rascal!'
+# ELSE 
+# PRINT 'Not quite, but you can try again!'
+
+name = input('Want to guess what my name is? ')
+
+if name == 'TJ':
+    print('You got it ya rascal!')
+else:
+    print('Not quite, but you can try again!')
+
